faa832f709567a84f64bc43fae5f10da3a9de4e1
akki-sudo <akashc7619@gmail.com>
2025-01-14 19:50:19 +0530

    Update DelayDriver.c

--- DelayDriver.c
+++ DelayDriver.c
@@ -5,22 +5,22 @@ void delay_sec(unsigned int sec)
 	//int a[] = {15,60,30,15,15};
 	//unsigned int pclk = a[VPBDIV]*1000000;
 	
-	T0PC = 0;//starting point value of timer
-	T0PR = 15000000 -1;//ending point value of timer
-	T0TC = 0;//monitoring the condition 
+	T0PC = 0;//intial value where it's need to starts from
+	T0PR = 15000000 -1;//the value at it's need to end
+	T0TC = 0;
 	
-	T0TCR = 1;//start the timer 
+	T0TCR = 1; 
 	while( T0TC  < sec);
-	T0TCR = 0;//stop the timer 
+	T0TCR = 0;
 }
 void delay_ms(unsigned int ms)
 {
 	//int a[] = {15,60,30,15,15};
 	//unsigned int pclk = a[VPBDIV]*1000000;
 	
-	T0PC = 0;//starting point value of timer
-	T0PR = 15000 -1;//ending point value of timer
-	T0TC = 0;//monitoring the condition 
+	T0PC = 0;//intial value where it's need to starts from
+	T0PR = 15000 -1;//the value at it's need to end
+	T0TC = 0;
 	
 	T0TCR = 1;//start the timer 
 	while( T0TC  < ms);
